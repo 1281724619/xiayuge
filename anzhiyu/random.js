9aa15b46feba967b00e94b4688aef65708af04f5
1281724619 <1281724619@qq.com>
2024-03-31 16:24:19 +0800

    Site updated: 2024-03-31 16:24:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/31/【D129】【SLG安卓Live2D】かよちゃんとの特別補導 Android.Ver v1.0/"];function toRandomPost(){
+var posts=["2024/03/31/D129/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
