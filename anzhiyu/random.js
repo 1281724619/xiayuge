99ed18503da4776cdb75f4e6af8f8efab15c7a58
1281724619 <1281724619@qq.com>
2024-04-01 23:16:31 +0800

    Site updated: 2024-04-01 23:16:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/01/D131/","2024/03/31/D129/","2024/03/31/D130/"];function toRandomPost(){
+var posts=["2024/03/31/D129/","2024/03/31/D130/","2024/04/01/D131/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
